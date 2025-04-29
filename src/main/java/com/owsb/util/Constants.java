d96224bcf96c672bdecbf9394c171396d4b76b13
Huvee <wongrenthou1265@gmail.com>
2025-04-29 14:01:51 +0800

    added constant for text file

--- src/main/java/com/owsb/util/Constants.java
+++ src/main/java/com/owsb/util/Constants.java
@@ -14,10 +14,12 @@ public class Constants {
     // File paths for data files
     public static final String USER_FILE = DATA_DIR + File.separator + "users.txt";
     public static final String ITEM_FILE = DATA_DIR + File.separator + "items.txt";
+    public static final String INVENTORY_FILE = DATA_DIR + File.separator + "inventory.txt";
     public static final String SUPPLIER_FILE = DATA_DIR + File.separator + "suppliers.txt";
     public static final String SALES_FILE = DATA_DIR + File.separator + "sales.txt";
-    public static final String PR_FILE = DATA_DIR + File.separator + "requisitions.txt";
-    public static final String PO_FILE = DATA_DIR + File.separator + "orders.txt";
+    public static final String PR_FILE = DATA_DIR + File.separator + "purchase_requisitions.txt";
+    public static final String PO_FILE = DATA_DIR + File.separator + "purchase_orders.txt";
+    public static final String PAYMENTS_FILE = DATA_DIR + File.separator + "payments.txt";
     
     // Ensure data directory exists
     static {
